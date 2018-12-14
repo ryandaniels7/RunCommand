d1d39a287c2f5afb4c553f1976bc6a47620e6157
ryan.daniels@questrail.com <ryan.daniels@questrail.com>
2018-12-14 08:14:21 -0600

    First Commit

--- RunCommand/Form1.Designer.cs
+++ RunCommand/Form1.Designer.cs
@@ -28,13 +28,96 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.TextEntry = new System.Windows.Forms.TextBox();
+            this.RunButton = new System.Windows.Forms.Button();
+            this.oCancelButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TextEntry
+            // 
+            this.TextEntry.AutoCompleteCustomSource.AddRange(new string[] {
+            "C",
+            "CALC",
+            "CHROME",
+            "CLEAN",
+            "DOS",
+            "DOSCONFIG",
+            "EDM",
+            "EXCEL",
+            "FINAL",
+            "FIREFOX",
+            "HELP",
+            "LADDER",
+            "MLOK",
+            "NOTE",
+            "PROGRESS",
+            "SATS",
+            "SCAN",
+            "SDU",
+            "TIME",
+            "TOBE",
+            "ULTI",
+            "VIS",
+            "WORD"});
+            this.TextEntry.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.TextEntry.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+            this.TextEntry.Location = new System.Drawing.Point(12, 12);
+            this.TextEntry.Name = "TextEntry";
+            this.TextEntry.Size = new System.Drawing.Size(131, 20);
+            this.TextEntry.TabIndex = 0;
+            this.TextEntry.TextChanged += new System.EventHandler(this.TextEntry_TextChanged);
+            // 
+            // RunButton
+            // 
+            this.RunButton.AutoSize = true;
+            this.RunButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.RunButton.Location = new System.Drawing.Point(104, 38);
+            this.RunButton.Name = "RunButton";
+            this.RunButton.Size = new System.Drawing.Size(39, 23);
+            this.RunButton.TabIndex = 1;
+            this.RunButton.Text = "Run";
+            this.RunButton.UseVisualStyleBackColor = true;
+            this.RunButton.Click += new System.EventHandler(this.RunButton_Click);
+            // 
+            // oCancelButton
+            // 
+            this.oCancelButton.AutoSize = true;
+            this.oCancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.oCancelButton.Location = new System.Drawing.Point(48, 38);
+            this.oCancelButton.Name = "oCancelButton";
+            this.oCancelButton.Size = new System.Drawing.Size(50, 23);
+            this.oCancelButton.TabIndex = 2;
+            this.oCancelButton.Text = "Cancel";
+            this.oCancelButton.UseVisualStyleBackColor = true;
+            this.oCancelButton.Click += new System.EventHandler(this.CancelButton_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.RunButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.AutoSize = true;
+            this.CancelButton = this.oCancelButton;
+            this.ClientSize = new System.Drawing.Size(154, 71);
+            this.Controls.Add(this.oCancelButton);
+            this.Controls.Add(this.RunButton);
+            this.Controls.Add(this.TextEntry);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox TextEntry;
+        private System.Windows.Forms.Button RunButton;
+        private System.Windows.Forms.Button oCancelButton;
     }
 }
 
--- RunCommand/Form1.cs
+++ RunCommand/Form1.cs
@@ -1,20 +1,179 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 using System.Windows.Forms;
 
 namespace RunCommand
 {
     public partial class Form1 : Form
     {
+        public string oCurrent;
+        public string oLast;
+        public string oShared;
+        public bool oStack;
+        public bool oFailed;
         public Form1()
         {
             InitializeComponent();
+        }
+        public void oHandle(string oErrorMessage)
+        {
+            MessageBox.Show(oErrorMessage);
+            TextEntry.Text = "";
+            oFailed = true;
+        }
+        public void oProcess(string oCommand)
+        {
+            try
+            {
+                Process.Start(oCommand);
+            }
+            catch (Exception oError)
+            {
+                oHandle(oError.Message);
+            }
+        }
+        public void RunCommand()
+        {
+            oShared = @"S:\";
+            oFailed = false;
+            oCurrent = TextEntry.Text.ToUpper().Trim();
+            if (oCurrent == "")
+            {
+                Dispose();
+            }
+            else if (oCurrent == "C")
+            {
+                oProcess(@"C:\");
+            }
+            else if (oCurrent == "CALC")
+            {
+                oProcess(@"calc.exe");
+            }
+            else if (oCurrent == "DOS")
+            {
+                oProcess(@"C:\Program Files (x86)\DOSBox SVN-Daum\dosbox.lnk");
+            }
+            else if (oCurrent == "DOSCONFIG")
+            {
+                oProcess(@"C:\Program Files (x86)\DOSBox SVN-Daum\TOOLS\Run DOSBox configuration.bat");
+            }
+            else if (oCurrent == "SCAN")
+            {
+                oProcess(@"C:\Program Files (x86)\NAPS2\NAPS2.exe");
+            }
+            else if (oCurrent == "VIS")
+            {
+                oProcess(@"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv.exe");
+            }
+            else if (oCurrent == "SATS")
+            {
+                oProcess(@"C:\Users\Ryan Daniels\Documents\Shortcuts\SATS 1.rdp");
+            }
+            else if (oCurrent == "PROGRESS")
+            {
+                oProcess( oShared + @"PROGRAMS\In Progress\");
+            }
+            else if (oCurrent == "MLOK")
+            {
+                oProcess(@"C:\Users\Ryan Daniels\Documents\Visual Studio 2017\Projects\OmahaUtilities\MicrolokTools\MicrolokTools\bin\Debug\MicrolokTools.exe");
+            }
+            else if (oCurrent == "FINAL")
+            {
+                oProcess(oShared + @"PROGRAMS\Final\");
+            }
+            else if (oCurrent == "TO BE")
+            {
+                oProcess(oShared + @"PROGRAMS\To Be Tested\");
+            }
+            else if (oCurrent == "SDU")
+            {
+                oProcess(oShared + @"DOCUMENTS\SDU CALC.xlsx");
+            }
+            else if (oCurrent == "HELP")
+            {
+                oProcess(@"C:\Users\Ryan Daniels\Documents\Commands.txt");
+            }
+            else if (oCurrent == "LADDER")
+            {
+                oProcess(@"C:\Utilities\LadderView.exe");
+            }
+            else if (oCurrent == "ULTI")
+            {
+                oProcess(@"https://n11.ultipro.com/Login.aspx?ReturnUrl=%2f");
+                System.Threading.Thread.Sleep(2000);
+                SendKeys.SendWait("{ENTER}");
+            }
+            else if (oCurrent == "EDM")
+            {
+                try
+                {
+                    Process.Start(@"IExplore.exe", "https://employees.www.uprr.com/emp/engineering/apps/secure/efms/edm/index.cfm");
+                }
+                catch (Exception oError)
+                {
+                    oHandle(oError.Message);
+                }
+                System.Threading.Thread.Sleep(1000);
+                SendKeys.SendWait("xcng813");
+                SendKeys.SendWait("{TAB}");
+                SendKeys.SendWait("quest50c");
+                SendKeys.SendWait("{ENTER}");
+            }
+            else if (oCurrent == "TIME")
+            {
+                oProcess(@"https://secure.tickspot.com/");
+            }
+            else if (oCurrent == "CHROME")
+            {
+                oProcess(@"chrome.exe");
+            }
+            else if (oCurrent == "FIREFOX")
+            {
+                oProcess(@"firefox.exe");
+            }
+            else if (oCurrent == "EXCEL")
+            {
+                oProcess(@"excel.exe");
+            }
+            else if (oCurrent == "WORD")
+            {
+                oProcess(@"winword.exe");
+            }
+            else if (oCurrent == "NOTE")
+            {
+                oProcess(@"notepad.exe");
+            }
+            else if (oCurrent == "CLEAN")
+            {
+                oProcess(@"ccleaner.exe");
+            }
+            else if (oStack = true && oCurrent == oLast)
+            {
+                oProcess("https://www.google.com/search?q=stackoverflow+C%23+" + oCurrent.Replace(" ", "+"));
+            }
+            else
+            {
+                oStack = true;
+                oLast = oCurrent;
+                return;
+            }
+            if (oFailed == false)
+            {
+                Dispose();
+            }
+        }
+        private void RunButton_Click(object sender, EventArgs e)
+        {
+            RunCommand();
+        }
+        private void TextEntry_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            Dispose();
         }
     }
 }
